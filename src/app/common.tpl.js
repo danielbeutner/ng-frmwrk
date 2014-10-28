36c84278ceed7fcc50ceead38593e104ff2152ca
Daniel Ditgens <daniel@ditgens.net>
2014-10-28 10:36:05 +0100

    Added flexbox grid

--- src/app/common.tpl.js
+++ src/app/common.tpl.js
@@ -1,14 +1,2 @@
-angular.module('frmwrk.common.tpl', ['common/elements/grid/grid.jade', 'common/modules/grid/grid.jade']);
+angular.module('frmwrk.common.tpl', []);
 
-angular.module("common/elements/grid/grid.jade", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("common/elements/grid/grid.jade",
-    "<h1>grid</h1>\n" +
-    "<div>Please add a tempalte to your new tag grid</div>");
-}]);
-
-angular.module("common/modules/grid/grid.jade", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("common/modules/grid/grid.jade",
-    "<div>Inner content:\n" +
-    "  <div ng-transclude=\"ng-transclude\"></div>\n" +
-    "</div>");
-}]);
